f8caf22b97324170929bfec9e9b5c9aafc94c4bd
Vladead <vladead8@gmail.com>
2025-08-02 22:05:05 +0300

    feat(skeleton): base dependencies

---
+++ src/main/java/com/vire/virebackend/ViReBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vire.virebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViReBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViReBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vire/virebackend/ViReBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vire.virebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViReBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
