a1f02dd101656de3b7b34d521d93dc092f87dc42
Vladead <vladead8@gmail.com>
2025-08-16 21:54:22 +0300

    refactor(config): switch to EnableConfigurationProperties for more predictable behaviour

--- src/main/java/com/vire/virebackend/ViReBackendApplication.java
+++ src/main/java/com/vire/virebackend/ViReBackendApplication.java
@@ -1,13 +1,14 @@
 package com.vire.virebackend;
 
+import com.vire.virebackend.config.CorsProperties;
 import com.vire.virebackend.config.JwtProperties;
 import com.vire.virebackend.problem.ProblemProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan(basePackageClasses = {JwtProperties.class, ProblemProperties.class})
+@EnableConfigurationProperties({JwtProperties.class, ProblemProperties.class, CorsProperties.class})
 public class ViReBackendApplication {
 
     public static void main(String[] args) {
