78e5dba385a433d0eaf0eb8a135dc95993eded8c
Vladead <vladead8@gmail.com>
2025-08-17 23:03:47 +0300

    feat(SessionService): implement base create/list/end session logic
    Refs #7

--- src/main/java/com/vire/virebackend/repository/SessionRepository.java
+++ src/main/java/com/vire/virebackend/repository/SessionRepository.java
@@ -4,8 +4,14 @@ import com.vire.virebackend.entity.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SessionRepository extends JpaRepository<Session, UUID> {
+
+    List<Session> findAllByUserIdOrderByCreatedAtDesc(UUID userId);
+
+    Optional<Session> findByIdAndUserId(UUID id, UUID userId);
 }
