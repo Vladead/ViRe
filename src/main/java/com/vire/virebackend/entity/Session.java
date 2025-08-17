e093a56ebb8c52349d993cd68b7dfbe00d313384
Vladead <vladead8@gmail.com>
2025-08-17 22:15:54 +0300

    feat(Session): add Session entity and repository
    Refs #7

---
+++ src/main/java/com/vire/virebackend/entity/Session.java
@@ -0,0 +1,42 @@
+package com.vire.virebackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "sessions")
+public class Session {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "device_name", length = 100)
+    private String deviceName;
+
+    @Column(name = "ip", length = 45)
+    private String ip;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "expires_at")
+    private LocalDateTime expiresAt;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+}
