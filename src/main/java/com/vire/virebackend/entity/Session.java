c3449c6707d90d72e8269d62f7a0c9d5b32b7915
Vladead <vladead8@gmail.com>
2025-08-17 23:37:10 +0300

    feat(SessionController): add create/list/delete endpoints
    Refs #7

--- src/main/java/com/vire/virebackend/entity/Session.java
+++ src/main/java/com/vire/virebackend/entity/Session.java
@@ -3,6 +3,7 @@ package com.vire.virebackend.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "sessions")
 public class Session {
     @Id
