e75c7c74a67bf3c38cacfc97c5606b9b32ee7678
Vladead <vladead8@gmail.com>
2025-08-19 18:59:29 +0300

    feat(Users): add version
    Refs #8

--- src/main/java/com/vire/virebackend/entity/User.java
+++ src/main/java/com/vire/virebackend/entity/User.java
@@ -3,6 +3,7 @@ package com.vire.virebackend.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 import lombok.*;
 
 @Entity
@@ -26,4 +27,9 @@ public class User extends BaseEntity {
     @Builder.Default
     @Column(name = "is_active", nullable = false)
     private Boolean isActive = true;
+
+    @Version
+    @Setter(AccessLevel.NONE)
+    @Column(name = "version", nullable = false)
+    private Long version;
 }
